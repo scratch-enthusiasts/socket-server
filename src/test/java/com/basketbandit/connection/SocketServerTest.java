2d65df5fff672cf8540ced30f637547b3917fcdc
BasketBandit <errorsfound@hotmail.co.jp>
2021-07-18 01:41:38 +0100

    add database and socket server cycling test

--- src/test/java/com/basketbandit/connection/SocketServerTest.java
+++ src/test/java/com/basketbandit/connection/SocketServerTest.java
@@ -1,32 +1,35 @@
 package com.basketbandit.connection;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import ch.vorburger.exec.ManagedProcessException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SocketServerTest {
-    private SocketServer server;
 
-    @BeforeEach
-    void setUp() throws IOException {
-        server = new SocketServer(9999);
-    }
-
-    @AfterEach
-    void tearDown() {
+    @Test
+    void socketCycle() throws IOException {
+        SocketServer server = new SocketServer(9999);
         server.shutdown();
     }
 
     @Test
-    void generatePasscode() {
+    void databaseCycle() throws ManagedProcessException {
+        DatabaseServer database = new DatabaseServer(3306);
+        database.start();
+        database.shutdown();
+    }
+
+    @Test
+    void generatePasscode() throws IOException {
+        SocketServer server = new SocketServer(9999);
+
         final String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         assert server.generatePasscode().length() == 5;
         for(String character : server.generatePasscode().split("")) {
             assert chars.contains(character);
         }
+
+        server.shutdown();
     }
 }
